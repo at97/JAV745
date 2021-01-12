5e17cb891bd444ee6881366e743265cf4e52fc52
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-12 02:34:30 -0500

    Solutions for week01 lab

---
+++ week01/employeeSalaryApp.java
@@ -0,0 +1,14 @@
+package week01;
+
+/**
+ * Develop a program that calculates an hourly paid worker gross salary and net
+ * salary. You are to assume that employees in this fictional scenario are paid
+ * per week and the only deduction that all employees incur is a 25% tax on
+ * their gross salary.
+ */
+public class employeeSalaryApp {
+
+    public static void main(String[] args) {
+
+    }
+}
