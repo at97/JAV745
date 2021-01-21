d4da33966ca0d8d106ba7c6ff020aaf033ab699a
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-20 19:51:59 -0500

    Lab 02 Q2

---
+++ labs/lab02/src/QuestionTwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class QuestionTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if(age > 18) {
+            System.out.println("You have been granted access");
+        }
+    }
+}
