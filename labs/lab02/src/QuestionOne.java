f8631ae4f58fced20382fc847e6d997b15d5ed0f
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-20 19:42:25 -0500

    Lab 02 Q2

---
+++ labs/lab02/src/QuestionOne.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class QuestionOne {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if(age <= 18) {
+            System.out.println("You are too young to go beyond this point.");
+        }
+    }
+}
