f90ef50e2d593a355441bc57c344af27b2119bae
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-23 00:09:34 -0500

    Add while loop

--- labs/lab03/src/Question01.java
+++ labs/lab03/src/Question01.java
@@ -1,7 +1,14 @@
 public class Question01 {
     public static void main(String[] args) {
+        System.out.println("For loop:");
         for (int i = 0; i < 10; i++) {
             System.out.println("I love programming.");
+        }
+        System.out.println("\nWhile loop:");
+        int counter = 0;
+        while (counter < 10) {
+            System.out.println("I love programming.");
+            counter++;
         }
     }
 }
