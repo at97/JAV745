69e21f69eb61f6186f527a7a2687df278f35221b
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-23 00:56:00 -0500

    Add constant

--- labs/lab03/src/Question01.java
+++ labs/lab03/src/Question01.java
@@ -2,7 +2,7 @@ public class Question01 {
     public static void main(String[] args) {
         // Declare constant
         final int LIMIT = 10;
-        
+
         System.out.println("For loop:");
         for (int i = 0; i < 10; i++) {
             System.out.println("I love programming.");
