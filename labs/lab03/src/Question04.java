86c71baa11d66583c9b9ee16af6cbc7cf64dc8fd
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-23 02:39:31 -0500

    Lab3 Q4

---
+++ labs/lab03/src/Question04.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Question04 {
+    public static void main(String[] args) {
+        // Declare scanner
+        Scanner read = new Scanner(System.in);
+
+        // Declare constant
+        final int LIMIT = 6;
+
+        int counter = 0;
+        int numPositive = 0;
+        int sumNegative = 0;
+        while (counter < LIMIT) {
+            System.out.print("Enter an integer: ");
+            int num = read.nextInt();
+
+            if (num > 0) {
+                numPositive++;
+            } else {
+                sumNegative += num;
+            }
+            counter++;
+        }
+        System.out.println("Number of positive number: " + numPositive);
+        System.out.println("Sum of negative numbers: " + sumNegative);
+    }
+}
