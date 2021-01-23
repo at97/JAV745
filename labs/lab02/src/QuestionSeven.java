fa2821d14a972441c49a83a452db8deef8568938
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-22 22:39:32 -0500

    Update user interaction

--- labs/lab02/src/QuestionSeven.java
+++ labs/lab02/src/QuestionSeven.java
@@ -5,6 +5,10 @@ public class QuestionSeven {
         // Create a scanner
         Scanner read = new Scanner(System.in);
 
+        // Constants
+        final int AGE_LIMIT = 22;
+        final int MINIMUM_CARS = 0;
+
         // Prompt user for inputs
         System.out.print("Hi, what's your name? ");
         String name = "";
@@ -13,23 +17,25 @@ public class QuestionSeven {
 
         System.out.print("How old are you? ");
         int age = read.nextInt();
-        System.out.println("Hmmmm, you don't look a day over " + age + ".");
+        int complimentAge = age - 5;
 
-        if (age > 22) {
+        if (age > AGE_LIMIT) {
+            System.out.println("Hmmmm, you don't look a day over " + complimentAge + ".");
             System.out.print("Tell me, " + name + ", how many cars do you own? ");
             int numCars = read.nextInt();
             // Read the leftover new line
             read.nextLine();
 
-            if (numCars > 0) {
+            if (numCars > MINIMUM_CARS) {
                 System.out.print(name + ", what is the brand model of your car? Input: ");
                 String car = "";
                 car += read.nextLine();
                 System.out.println(car + " is a great choice for a car.\n" + name + ", have a great day. Good bye!");
             } else {
-                System.out.println("Wow, come on now " + name + ", you are " + age + " and do not own a car!");
+                System.out.println("Wow, come on now " + name + ", you are " + age + " and don't own a car?!");
             }
-
+        } else {
+            System.out.println("Sorry, you don't meet the minimum required age to continue.");
         }
     }
 }
