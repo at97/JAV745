9f82dad2b45a3a0fc79b45c1dfee0b7d0757f036
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-25 19:08:17 -0500

    Correct typo

--- labs/lab04/src/Question04.java
+++ labs/lab04/src/Question04.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Question03 {
+public class Question04 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
 
-        System.out.print("Enter a number to square: ");
+        System.out.print("Enter a number to sqaure: ");
         double num = read.nextDouble();
 
         // Invoke method
