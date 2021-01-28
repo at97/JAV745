4e18f6ef0de2b920f55732578dfc827238b5e72d
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-01-28 01:10:59 -0500

    Solution for Q2

--- labs/lab05/src/Question02.java
+++ labs/lab05/src/Question02.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Question02 {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        final int ARRAY_LENGTH = 5;
+        double[] array = new double[ARRAY_LENGTH];
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter a number: ");
+            array[i] = read.nextDouble();
+        }
+
+        // Print all numbers
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        double smallestNum = smallestNumber(array);
+        System.out.println("The smallest number is " + smallestNum);
+
+    }
+
+    public static double smallestNumber(double[] array) {
+        double smallestNum = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallestNum) {
+                smallestNum = array[i];
+            }
+        }
+        return smallestNum;
     }
 }
