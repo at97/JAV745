736af400596c22b5c096bfed50e6b18c4ad72bd6
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-02-28 00:38:45 -0500

    Add constructor

---
+++ labs/lab06/src/questionThree/StoreClerk.java
@@ -0,0 +1,16 @@
+package questionThree;
+
+public class StoreClerk {
+    private String firstName;
+    private String lastName;
+    private int storeClerkID;
+
+    public StoreClerk() {
+    }
+
+    public StoreClerk(String firstName, String lastName, int storeClerkID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.storeClerkID = storeClerkID;
+    }
+}
